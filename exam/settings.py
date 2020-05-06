e7b4a31a2089a70ba9dff9967a2fa66172fa37a2
andrey <example@gmail.com>
2020-05-06 10:58:24 +0300

    Функция восстановления пароля

--- exam/settings.py
+++ exam/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'e&i374z9d51xdn1w*z#s6=zhljw%v!)_k8mx)ht!3$dq4u2k%-'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-LOGIN_REDIRECT_URL =  '/'
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -53,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'exam.urls'
-TEMPLATE_DIR = os.path.join(BASE_DIR ,'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -73,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'exam.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -83,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -115,13 +112,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='eliotlion73@gmail.com'
+EMAIL_HOST_PASSWORD='ddfgg5454'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
+    os.path.join(BASE_DIR, "static")
 ]
 MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
--- exam/urls.py
+++ exam/urls.py
@@ -19,10 +19,24 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path,include
 from posts import views
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as authViews
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
+
+
+    path('pass-reset/',authViews.PasswordResetView.as_view(template_name='pass_reset.html'),
+         name='pass-reset'),
+
+    path('password_reset_confirm/<uidb64>/<token>/',authViews.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password-reset_complete/',authViews.PasswordResetCompleteView.as_view(template_name='password-reset_complete.html'),
+         name='password-reset_complete'),
+
+    path('password-reset/done/',authViews.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done'),
+
     path('',include('examApp.urls')),
     path('',include('posts.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
